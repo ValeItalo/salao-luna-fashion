9db8fbbb76c911b771b6508277b2c92cf74d4a37
Ítalo Vale <italo.valesiii@gmail.com>
2024-04-07 18:53:31 -0300

    feature: correção da navegação e adicionada tooltip para indicar página em construção

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatTooltipModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
